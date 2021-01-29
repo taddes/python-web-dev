6a8a4a54db791dd02865e567a35828bb4801a284
Taddes Korris <taddes.korris@gmail.com>
2021-01-29 17:59:53 -0500

    price

--- 2. Refresh/destructure_vars.py
+++ 2. Refresh/destructure_vars.py
@@ -30,6 +30,12 @@ class Book:
     def __repr__(self):
         return f'Book {self.name}, {self.book_type}, weighing {self.weight}'
 
+    @classmethod
+    def hardcover(cls, name, page_weight):
+        return Book(name, Book.TYPES[0], page_weight + 100)
+
 book = Book('Harry Potter', 'Fantasy', '4')
 
 print(book)
+newbook = Book.hardcover('Harry Potter', 2000)
+print(newbook)
